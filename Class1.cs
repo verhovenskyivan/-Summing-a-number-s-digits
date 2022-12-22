ed8378dd7db3b37b50554f0d14465bb1aee28065
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-22 22:19:01 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+    public static int SumDigits(int n)
+    {
+        n = Math.Abs(n);
+        int sum = 0;
+        while (n != 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
